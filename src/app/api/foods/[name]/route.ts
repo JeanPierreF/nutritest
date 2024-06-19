2540b82d0c3df1c6761e61757839a7f3f8e95e52
JeanPierreF <jeanpierrefrancois49@gmail.com>
2024-06-19 18:56:07 +0200

    product page

--- src/app/api/foods/[name]/route.ts
+++ src/app/api/foods/[name]/route.ts
@@ -1,5 +1,29 @@
 import { foods } from "@/src/data";
 
-export async function GET() {
-    return Response.json(foods);
+export async function GET(
+    request: Request,
+    { params }: { params: { name: string }}
+) {
+    console.log('ok');
+    console.log(params.name);
+    const index = foods.findIndex(
+        (food) => food.name.toLowerCase().replace(/ /g, '-') === params.name
+    )
+    console.log(foods[index]);
+    if(index !== -1) {
+        return new Response(JSON.stringify(foods[index]), {
+            headers: {
+                "Content-Type": "application/json"
+            },
+            status: 200
+        })
+    }
+    else {
+        return new Response("Food not found.", {
+            headers: {
+                "Content-Type": "application/json"
+            },
+            status: 404
+        });
+    } 
 }
